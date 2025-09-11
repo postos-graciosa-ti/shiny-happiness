ee24ec5b99bd821ca21f918c7fc5c6573ad799fa
postos-graciosa-ti <postosgraciosati@gmail.com>
2025-09-11 16:39:45 -0300

    feature: add workjourney doc

--- src/routes/turns.py
+++ src/routes/turns.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from src.controllers.turns import handle_get_turns
+from src.controllers.turns import handle_get_turns, handle_get_turns_by_id
 from src.security.verify_jwt_token import verify_jwt_token
 
 turns_routes = APIRouter(dependencies=[Depends(verify_jwt_token)])
@@ -9,3 +9,8 @@ turns_routes = APIRouter(dependencies=[Depends(verify_jwt_token)])
 @turns_routes.get("/turns")
 async def get_turns():
     return await handle_get_turns()
+
+
+@turns_routes.get("/turns/{id}")
+async def get_turns_by_id(id: int):
+    return await handle_get_turns_by_id(id)
